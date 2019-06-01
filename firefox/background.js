ecaddbc3cfe3634a2030a0b55f3f8706250ab5ab
Wesley Branton <addons@wesleybranton.com>
2019-06-01 12:23:43 -0400

    Added storage initialization

---
+++ firefox/background.js
@@ -0,0 +1,13 @@
+browser.storage.local.set({'lastEmail':''});
+let data = browser.storage.local.get();
+data.then(verify);
+
+// Verify setting validity
+function verify(info) {
+	if (info.doNotAsk && (info.mode != 'live' || info.mode != 'office')) {
+		browser.storage.local.set({
+			mode: '',
+			doNotAsk: false
+		});
+	}
+}
