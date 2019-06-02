1f5bb6cc3dc70f403ddeef7052765b3035f4030c
Wesley Branton <addons@wesleybranton.com>
2019-06-02 00:33:34 -0400

    Added license headers

--- firefox/background.js
+++ firefox/background.js
@@ -1,5 +1,6 @@
-let data = browser.storage.local.get();
-data.then(verify);
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 // Verify setting validity
 function verify(info) {
@@ -38,6 +39,8 @@ function listenMessage(message) {
 	}
 }
 
+let data = browser.storage.local.get();
+data.then(verify);
 var tmpUrl;
 const filter = {urls:["*://outlook.live.com/mail/deeplink/compose",
 	"*://outlook.office.com/mail/deeplink/compose"]};
