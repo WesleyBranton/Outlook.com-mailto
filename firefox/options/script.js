2e027442cca5b5c6fb4be2ec6426e5625ad13991
Wesley Branton <addons@wesleybranton.com>
2019-11-23 21:33:58 -0500

    Updated options UI

--- firefox/options/script.js
+++ firefox/options/script.js
@@ -5,9 +5,9 @@
 // Load settings
 function restore(setting) {
 	if (setting.mode != 'live' && setting.mode != 'office') {
-		settingMode.value = 'ask';
+		document.settings.mode.value = 'ask';
 	} else {
-		settingMode.value = setting.mode;
+		document.settings.mode.value = setting.mode;
 	}
 }
 
@@ -18,7 +18,6 @@ function save() {
 	});
 }
 
-var settingMode = document.getElementById('mode');
 let data = browser.storage.local.get();
 data.then(restore);
-document.getElementsByTagName("form")[0].addEventListener("change", save);
+document.settings.addEventListener('change', save);
